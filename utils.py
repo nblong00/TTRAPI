351fa53ea74c1008b5b1e79570a1147b3bf5fa42
Blaise <blazzze0517@gmail.com>
2025-05-26 11:16:02 -0400

    Made CSV creation and write functions generic for use across multiple scripts in the future.

--- utils.py
+++ utils.py
@@ -13,25 +13,22 @@ def dt():
     dt_string = datetime.datetime.strftime(current_dt, "%I:%M%p on %m-%d-%Y")
     return dt_string
 
-def convert_epoch_timestamp(data):
+
+def convert_epoch_timestamp_string(data):
     converted_epoch_timestamp = datetime.datetime.fromtimestamp(data["lastUpdated"])
     dt_string = datetime.datetime.strftime(converted_epoch_timestamp, "%I:%M%p on %m-%d-%Y")
     return dt_string
 
-def export_cleanedup_CSV_and_import(data):
-    with open('adjustedData.csv', 'w+', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        writer = csv.writer(csvfile, delimiter=',', lineterminator='')
-        writer.writerow(['DistrictName', 'Type', 'Progress'])
+
+def create_CSV_for_data(column_names):
+    with open("adjustedData.csv", "w+", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=",", lineterminator="")
+        writer.writerow(column_names)
         csv_new_line(csvfile)
-        for district in data['invasions']:
-            if 'Tele\u0003marketer' in data['invasions'][district]['type']:
-                data['invasions'][district]['type'] = "Telemarketer" 
-            elif 'Micro\u0003manager' in data['invasions'][district]['type']:
-                data['invasions'][district]['type'] = "Micromanager"
-            else:
-                pass
-            writer.writerow([district, 
-                            data['invasions'][district]['type'],
-                            data['invasions'][district]['progress']])
-            csv_new_line(csvfile)
+
+
+def write_data_to_CSV(write_data):
+    with open("adjustedData.csv", "a+", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=",", lineterminator="")
+        writer.writerow(write_data)
+        csv_new_line(csvfile)
