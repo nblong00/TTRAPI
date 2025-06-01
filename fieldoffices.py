704ffde3fd8a6b03ec87504f7e643b6bcf1a34f2
Blaise <blazzze0517@gmail.com>
2025-05-31 22:32:16 -0400

    Writing desired data into CSV for dataframe loading.

--- fieldoffices.py
+++ fieldoffices.py
@@ -5,8 +5,21 @@ ZONE_ID_LOOKUP = {"3100":"Walrus Way","3200":"Sleet Street","3300":"Polar Place"
 ENDPOINT = "(FOS)"
 
 data = utils.error_checking_and_logging(URL, ENDPOINT)
-utils.create_CSV_for_data(["Location","Difficulty", "Annexes", "Open?"])
+utils.create_CSV_for_data(["Location","Difficulty", "Open?", "Annexes"])
 
 for street in data["fieldOffices"]:
     street_name = ZONE_ID_LOOKUP[street]
+    if data["fieldOffices"][street]["difficulty"] in range(1, 4):
+        difficulty_rating = f"{data["fieldOffices"][street]["difficulty"]} stars"
+    elif data["fieldOffices"][street]["difficulty"] == 0:
+        difficulty_rating = f"No stars"
+    if data["fieldOffices"][street]["open"]:
+        open_status = "Yes"
+    elif not data["fieldOffices"][street]["open"]:
+        open_status = "No"
+    if data["fieldOffices"][street]["annexes"] > 0:
+        annexes_left = f"{data["fieldOffices"][street]["annexes"]} remaining"
+    else:
+        annexes_left = "Field Office closing"
+    utils.write_data_to_CSV([street_name, difficulty_rating, open_status, annexes_left])
     
