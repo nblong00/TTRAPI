470ccae7739b5af22c7a99dcbf1a4568ef03c4d1
Blaise <blazzze0517@gmail.com>
2025-05-31 22:18:35 -0400

    Setup CSV creation. Also setup Zone ID translation per entry in field offices response.

--- fieldoffices.py
+++ fieldoffices.py
@@ -1,7 +1,12 @@
 import utils
 
 URL = "https://www.toontownrewritten.com/api/fieldoffices"
+ZONE_ID_LOOKUP = {"3100":"Walrus Way","3200":"Sleet Street","3300":"Polar Place","4100":"Alto Avenue","4200":"Baritone Boulevard","4300":"Tenor Terrace","5100":"Elm Street","5200":"Maple Street","5300":"Oak Street","9100":"Lullaby Lane","9200":"Pajama Place"}
 ENDPOINT = "(FOS)"
 
 data = utils.error_checking_and_logging(URL, ENDPOINT)
-print(data)
+utils.create_CSV_for_data(["Location","Difficulty", "Annexes", "Open?"])
+
+for street in data["fieldOffices"]:
+    street_name = ZONE_ID_LOOKUP[street]
+    
