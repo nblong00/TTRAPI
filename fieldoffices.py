73b13c567d6389b6c0c10b49635e92b504471aaa
Blaise <blazzze0517@gmail.com>
2025-05-31 22:40:16 -0400

    Reading CSV containing current field offices data. Displaying it as dataframe to user.

--- fieldoffices.py
+++ fieldoffices.py
@@ -1,4 +1,5 @@
 import utils
+import pandas
 
 URL = "https://www.toontownrewritten.com/api/fieldoffices"
 ZONE_ID_LOOKUP = {"3100":"Walrus Way","3200":"Sleet Street","3300":"Polar Place","4100":"Alto Avenue","4200":"Baritone Boulevard","4300":"Tenor Terrace","5100":"Elm Street","5200":"Maple Street","5300":"Oak Street","9100":"Lullaby Lane","9200":"Pajama Place"}
@@ -30,6 +31,8 @@ def main():
     data = utils.error_checking_and_logging(URL, ENDPOINT)
     utils.create_CSV_for_data(["Location","Difficulty", "Open?", "Annexes"])
     logic_loop(data)
+    result = pandas.read_csv("AdjustedData.csv")
+    print(result)
 
 
 main()    
