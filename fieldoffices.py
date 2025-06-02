43afd989c7294fb0e372be6e4098c7ec534bb814
Blaise <blazzze0517@gmail.com>
2025-06-01 20:51:47 -0400

    Added logic to display 'neighborhood' that field office is located in.

--- fieldoffices.py
+++ fieldoffices.py
@@ -3,12 +3,14 @@ import pandas
 
 URL = "https://www.toontownrewritten.com/api/fieldoffices"
 ZONE_ID_LOOKUP = {"3100":"Walrus Way","3200":"Sleet Street","3300":"Polar Place","4100":"Alto Avenue","4200":"Baritone Boulevard","4300":"Tenor Terrace","5100":"Elm Street","5200":"Maple Street","5300":"Oak Street","9100":"Lullaby Lane","9200":"Pajama Place"}
+ID_TO_NEIGHBORHOOD = {"3100":"The Brrrgh","3200":"The Brrrgh","3300":"The Brrrgh","4100":"Minne's Melodyland","4200":"Minne's Melodyland","4300":"Minne's Melodyland","5100":"Daisy Gardens","5200":"Daisy Gardens","5300":"Daisy Gardens","9100":"Donald's Dreamland","9200":"Donald's Dreamland"}
 ENDPOINT = "(FOS)"
 
 
 def logic_loop(data):
     for street in data["fieldOffices"]:
         street_name = ZONE_ID_LOOKUP[street]
+        neighborhood = ID_TO_NEIGHBORHOOD[street]
         if data["fieldOffices"][street]["difficulty"] in range(1, 4):
             # Explict double space
             difficulty_rating = f"{data["fieldOffices"][street]["difficulty"]}  stars"
@@ -25,12 +27,12 @@ def logic_loop(data):
                 annexes_left = f"{data["fieldOffices"][street]["annexes"]}  remaining"
         else:
             annexes_left = "Field Office closing"
-        utils.write_data_to_CSV([street_name, difficulty_rating, open_status, annexes_left])
+        utils.write_data_to_CSV([neighborhood, street_name, difficulty_rating, open_status, annexes_left])
 
 
 def main():
     data = utils.error_checking_and_logging(URL, ENDPOINT)
-    utils.create_CSV_for_data(["Location","Difficulty", "Open?", "Annexes"])
+    utils.create_CSV_for_data(["Neighborhood", "Location", "Difficulty", "Open?", "Annexes"])
     logic_loop(data)
     result = pandas.read_csv("AdjustedData.csv")
     print(result)
