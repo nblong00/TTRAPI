b5af6d634a9b0d94c619c04d68bf8daad46f2f11
Blaise <blazzze0517@gmail.com>
2025-06-03 09:58:42 -0400

    Main now acts as 'main menu' and starting point, allowing user to run field office, invasions, or populations code.

---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+import populations
+import invasions
+import fieldoffices
+
+
+def menu_options():
+    failed_input = 1
+    options = ("\nEnter one of the below number options:" +
+              "\n1 - See active district populations" + 
+              "\n2 - See active invasions" +
+              "\n3 - See active field offices\n")
+    print(options)
+    while failed_input:
+        for attempt in range(5):
+            if attempt == 4:
+                print("Too many invalid entries. Program exiting...")
+                time.sleep(1)
+                exit()
+            user_input = input("> ")
+            if user_input not in ["1", "2", "3"]:
+                print(f"\nInvalid input. {options}")
+            else:
+                failed_input = 0
+                break
+    print()
+    return user_input
+
+
+def menu():
+    print("Welcome to the ToonTown Rewritten App.\n")
+    user_input = menu_options()
+    if user_input == "1":
+        populations.main()
+    elif user_input == "2":
+        invasions.main()
+    elif user_input == "3":
+        fieldoffices.main()
+
+menu()
