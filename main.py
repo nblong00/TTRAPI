6dae6a7667697d2340656e648dfb31f1d5b3f737
Blaise <blazzze0517@gmail.com>
2025-06-06 20:09:25 -0400

    Added logic to let user select new Silly Meter menu option.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import populations
 import invasions
 import fieldoffices
+import sillymeter
 
 
 def menu_options():
@@ -10,7 +11,8 @@ def menu_options():
               + "\n1 - See active district populations" 
               + "\n2 - See active invasions"
               + "\n3 - See active field offices"
-              + "\n4 - EXIT APP\n")
+              + "\n4 - Check if Silly Meter is active"
+              + "\n5 - EXIT APP\n")
     print(options)
     while failed_input:
         for attempt in range(5):
@@ -19,7 +21,7 @@ def menu_options():
                 time.sleep(1)
                 exit()
             user_input = input("> ")
-            if user_input not in ["1", "2", "3", "4"]:
+            if user_input not in ["1", "2", "3", "4", "5"]:
                 print(f"\nInvalid input. {options}")
             else:
                 failed_input = 0
@@ -40,6 +42,8 @@ def menu():
             elif user_input == "3":
                 fieldoffices.main()
             elif user_input == "4":
+                sillymeter.main()
+            elif user_input == "5":
                 print("Thank you for using the ToonTown Rewritten App."
                     + "\nProgram closing...")
                 time.sleep(1)
