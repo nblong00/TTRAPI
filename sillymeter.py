6904af8978b7748af73d2f4cf9ed189c1d5f86cf
Blaise <blazzze0517@gmail.com>
2025-06-06 20:09:02 -0400

    Added sleep steps and included main logic in main()

--- sillymeter.py
+++ sillymeter.py
@@ -1,16 +1,18 @@
 import utils
+import time
 
 URL = "https://www.toontownrewritten.com/api/sillymeter"
 ENDPOINT = "(SIM)"
 
-
-data = utils.error_checking_and_logging(URL, ENDPOINT)
-# print(data)
-utils.create_CSV_for_data(["State", "Winner", "RewardDescription", "ProjectedEndTime"])
-
-if data["state"] == "Inactive":
-    print("=================================")
-    print("\nSilly Meter not currently active!\n")
-    print("=================================")
-else:
-    print(data["rewards"][data["winner"]])
+def main():
+    data = utils.error_checking_and_logging(URL, ENDPOINT)
+    # print(data)
+    utils.create_CSV_for_data(["State", "Winner", "RewardDescription", "ProjectedEndTime"])
+    time.sleep(0.5)
+    if data["state"] == "Inactive":
+        print("=================================")
+        print("\nSilly Meter not currently active!\n")
+        print("=================================")
+        time.sleep(0.5)
+    else:
+        print(data["rewards"][data["winner"]])
