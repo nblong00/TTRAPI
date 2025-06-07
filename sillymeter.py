8e1b4ea74824be8f6ae755a931613c8d68016c35
Blaise <blazzze0517@gmail.com>
2025-06-06 20:02:04 -0400

    Setup IF statement to test if silly meter is active as error checking.

--- sillymeter.py
+++ sillymeter.py
@@ -5,5 +5,12 @@ ENDPOINT = "(SIM)"
 
 
 data = utils.error_checking_and_logging(URL, ENDPOINT)
-print(data)
+# print(data)
 utils.create_CSV_for_data(["State", "Winner", "RewardDescription", "ProjectedEndTime"])
+
+if data["state"] == "Inactive":
+    print("=================================")
+    print("\nSilly Meter not currently active!\n")
+    print("=================================")
+else:
+    print(data["rewards"][data["winner"]])
