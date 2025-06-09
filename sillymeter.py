3189d7da858b763e9a150c224fbe0a7e05576519
Blaise <blazzze0517@gmail.com>
2025-06-08 20:15:17 -0400

    Changed from dataframe to standard print statements due to description length.

--- sillymeter.py
+++ sillymeter.py
@@ -6,6 +6,7 @@ URL = "https://www.toontownrewritten.com/api/sillymeter"
 ENDPOINT = "(SIM)"
 
 
+
 def main():
     data = utils.error_checking_and_logging(URL, ENDPOINT)
     if data["state"] == "Inactive":
@@ -13,17 +14,19 @@ def main():
         print("\nSilly Meter not currently active!\n")
         print("=================================")
         time.sleep(0.5)
+    elif data["state"] == "Active" and data['winner'] == None:
+        print("=================================")
+        print("Silly Meter is active but reward not decided!")
+        print("Potential Rewards & Descriptions:")
+        print("=================================\n")
         current_rewards(data)
-        result = pandas.read_csv("adjustedData.csv")
-        print(result)
-    else:
-        utils.create_CSV_for_data(["State", "Winner", "RewardDescription", "ProjectedEndTime"])
-        print(data["rewards"][data["winner"]])
+        # print(data)
+
 
 
 def current_rewards(data):
     i = 0
-    utils.create_CSV_for_data(["PotentialReward", "RewardDescription"])
     for reward in data["rewards"]:
-        utils.write_data_to_CSV([reward, data["rewardDescriptions"][i]])
+        print(reward + " | " + data["rewardDescriptions"][i])
         i += 1
+        
